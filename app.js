62f6984ed6515b2c98b170bdee8b3ed0d0ffc575
Chingsan0722 <rory85722@gmail.com>
2023-04-04 01:05:40 +0800

    remove node_modules & add mongoose

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//require body-parser here
+const bodyParser = require('body-parser')
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+//require mongoose here
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 //require express-handlebars here
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
@@ -22,7 +45,6 @@ app.get('/', (req, res) => {
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurants = restaurantList.results.find(restaurant => restaurant.id.toString().toLowerCase() === req.params.restaurant_id)
-  console.log('id', req.params.restaurant_id)
   res.render('show', { restaurant: restaurants })
 })
 
