2144adf52d93dae3010f899fa51858d2381f8007
Chingsan0722 <rory85722@gmail.com>
2023-04-17 11:36:35 +0800

    use standard JS

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-//require packages used in the project
+// require packages used in the project
 const express = require('express')
 const app = express()
 const port = 3000
 
-//require body-parser here
+// require body-parser here
 const bodyParser = require('body-parser')
 
-//method override
+// method override
 const methodOverride = require('method-override')
 
-//載入 Restaurant model
+// 載入 Restaurant model
 const Restaurant = require('./models/restaurant')
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-//require mongoose here
+// require mongoose here
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -32,45 +32,44 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-
-//require express-handlebars here
+// require express-handlebars here
 const exphbs = require('express-handlebars')
 const restaurant = require('./models/restaurant')
 // const restaurantList = require('./restaurant.json')
 
-//setting template engine here
+// setting template engine here
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-//setting static files 載入靜態檔案
+// setting static files 載入靜態檔案
 app.use(express.static('public'))
 
-//用app.use 規定每一筆請求都要透過body-parser進行前置處理
+// 用app.use 規定每一筆請求都要透過body-parser進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride("_method"))
+app.use(methodOverride('_method'))
 
-//index page
+// index page
 app.get('/', (req, res) => {
-  Restaurant.find() //取出Todo model裡的所有資料(也可在()中寫入指定要找的資料)
-    .lean() //把mongoose的model物件轉換成乾淨的 js資料陣列
-    .then(restaurantData => res.render('index', { restaurantData })) //將資料傳給 index樣板 , { restaurants } 是 { restaurants:restaurants }的縮寫
-    .catch(error => console.error(error)) //錯誤處理
+  Restaurant.find() // 取出Todo model裡的所有資料(也可在()中寫入指定要找的資料)
+    .lean() // 把mongoose的model物件轉換成乾淨的 js資料陣列
+    .then(restaurantData => res.render('index', { restaurantData })) // 將資料傳給 index樣板 , { restaurants } 是 { restaurants:restaurants }的縮寫
+    .catch(error => console.error(error)) // 錯誤處理
 })
-//show page & detail page
+// show page & detail page
 app.get('/restaurants/:restaurantId', (req, res) => {
   const { restaurantId } = req.params
   Restaurant.findById(restaurantId)
     .lean()
-    .then(restaurantData => res.render("show", { restaurantData }))
+    .then(restaurantData => res.render('show', { restaurantData }))
     .catch(error => console.log(error))
 })
 
-//edit page
+// edit page
 app.get('/restaurants/:restaurantId/edit', (req, res) => {
   const { restaurantId } = req.params
   Restaurant.findById(restaurantId)
     .lean()
-    .then(restaurantData => res.render("edit", { restaurantData }))
+    .then(restaurantData => res.render('edit', { restaurantData }))
     .catch(error => console.log(error))
 })
 app.post('/restaurants/:restaurantId/edit', (req, res) => {
@@ -85,18 +84,18 @@ app.post('/restaurants/:restaurantId/edit', (req, res) => {
   return Restaurant.findById(restaurantId)
     .then(restaurant => {
       restaurant.name = name,
-        restaurant.category = category,
-        restaurant.image = image,
-        restaurant.location = location,
-        restaurant.phone = phone,
-        restaurant.description = description
+      restaurant.category = category,
+      restaurant.image = image,
+      restaurant.location = location,
+      restaurant.phone = phone,
+      restaurant.description = description
       return restaurant.save()
     })
     .then(() => res.redirect(`/restaurants/${restaurantId}`))
     .catch(error => console.log(error))
 })
 
-//search function
+// search function
 app.get('/search', (req, res) => {
   if (!req.query.keyword) {
     res.redirect('/')
@@ -137,7 +136,7 @@ app.get('/search', (req, res) => {
 //   .catch(error => console.log(error))
 // })
 
-//delete restaurant
+// delete restaurant
 app.post('/restaurants/:restaurantId/delete', (req, res) => {
   const { restaurantId } = req.params
   return Restaurant.findById(restaurantId)
@@ -146,7 +145,7 @@ app.post('/restaurants/:restaurantId/delete', (req, res) => {
     .catch(error => console.log(error))
 })
 
-//start and listen express server
+// start and listen express server
 app.listen(port, () => {
   console.log(`now server begins running on http://localhost:${port}`)
 })
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant') // 載入 restaurant model
-const seedRestaurant = require('../../restaurant.json') //載入 種子餐廳
+const seedRestaurant = require('../../restaurant.json') // 載入 種子餐廳
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -15,7 +15,8 @@ db.once('open', () => {
   let i = 0
   while (i < seedRestaurant.results.length) {
     Restaurant.create(
-      { name: `${seedRestaurant.results[i].name}`,
+      {
+        name: `${seedRestaurant.results[i].name}`,
         name_en: `${seedRestaurant.results[i].name_en}`,
         category: `${seedRestaurant.results[i].category}`,
         image: `${seedRestaurant.results[i].image}`,
@@ -24,7 +25,7 @@ db.once('open', () => {
         google_map: `${seedRestaurant.results[i].google_map}`,
         rating: `${seedRestaurant.results[i].rating}`,
         description: `${seedRestaurant.results[i].description}`
-       },
+      }
     )
     i++
   }
