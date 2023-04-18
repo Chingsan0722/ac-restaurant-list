20429f8bf611fd52eaeff211e10406caa6720c92
Chingsan0722 <rory85722@gmail.com>
2023-04-18 12:29:20 +0800

    remove useless code

--- routes/modules/sort.js
+++ routes/modules/sort.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 // index page
 router.get('/nameasc', (req, res) => {
-  console.log(req)
   Restaurant.find() // 取出Todo model裡的所有資料(也可在()中寫入指定要找的資料)
     .lean() // 把mongoose的model物件轉換成乾淨的 js資料陣列
     .sort({ name: 'asc' })
@@ -13,36 +12,30 @@ router.get('/nameasc', (req, res) => {
 })
 
 router.get('/namedesc', (req, res) => {
-  if (Restaurant) {
     Restaurant.find() // 取出Todo model裡的所有資料(也可在()中寫入指定要找的資料)
       .lean() // 把mongoose的model物件轉換成乾淨的 js資料陣列
       .sort({ name: 'desc' })
       .then(restaurantData => res.render('index', { restaurantData })) // 將資料傳給 index樣板 , { restaurants } 是 { restaurants:restaurants }的縮寫
 
       .catch(error => console.error(error)) // 錯誤處理
-  }
 })
 
 router.get('/catasc', (req, res) => {
-  if (Restaurant) {
     Restaurant.find() // 取出Todo model裡的所有資料(也可在()中寫入指定要找的資料)
       .lean() // 把mongoose的model物件轉換成乾淨的 js資料陣列
       .sort({ category: 'asc' })
       .then(restaurantData => res.render('index', { restaurantData })) // 將資料傳給 index樣板 , { restaurants } 是 { restaurants:restaurants }的縮寫
 
       .catch(error => console.error(error)) // 錯誤處理
-  }
 })
 
 router.get('/locasc', (req, res) => {
-  if (Restaurant) {
     Restaurant.find() // 取出Todo model裡的所有資料(也可在()中寫入指定要找的資料)
       .lean() // 把mongoose的model物件轉換成乾淨的 js資料陣列
       .sort({ location: 'asc' })
       .then(restaurantData => res.render('index', { restaurantData })) // 將資料傳給 index樣板 , { restaurants } 是 { restaurants:restaurants }的縮寫
 
       .catch(error => console.error(error)) // 錯誤處理
-  }
 })
 
 module.exports = router
