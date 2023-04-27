f7221b3621cf178c40df4d6839d696db06e2f989
Chingsan0722 <rory85722@gmail.com>
2023-04-27 17:11:20 +0800

    feat: add middleware to verify user login or not

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const router = express.Router()
+require('../middleware/auth')
 
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const sort = require('./modules/sort')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/restaurants', restaurants)
-router.use('/search', search)
-router.use('/sort', sort)
+
+router.use('/restaurants', authenticator, restaurants)
+router.use('/search', authenticator, search)
+router.use('/sort', authenticator, sort)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
