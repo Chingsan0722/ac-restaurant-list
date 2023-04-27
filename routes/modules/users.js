781e1fcdaff0a09f8b8ec40bed31298d4c0c80f4
Chingsan0722 <rory85722@gmail.com>
2023-04-27 16:16:14 +0800

    feat: module users feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,3 +4,19 @@ const User = require('../../models/user')
 const passport = require('passport')
 // 雜湊密碼套件
 const bcrypt = require('bcryptjs')
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.get('/logout', (req, res) => {
+  req.logOut()
+  // req.flash('success_msg', '您已成功登出！')
+  res.redirect('/users/login')
+})
+
+module.exports = router
